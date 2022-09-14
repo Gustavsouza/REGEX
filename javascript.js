25ff759a7f019ee12d113fffe53f4c8f532c8673
Gustavo De Souza <74631630+Gustavsouza@users.noreply.github.com>
2022-09-14 14:19:17 -0300

    Máscara telefone

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@
 //   valida email aqui
 //   var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 function testInfo(phoneInput){
-var re = /^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/;
+var re = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/;
 var OK = re.exec(phoneInput.value);
 if (!OK){
     // window.alert(RegExp.input + " Não é um número de telefone com código de área!");
@@ -22,19 +22,41 @@ if (!OK){
 }
 
 // /*Valida email*/
-
-// var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-// function testInfo(emailvalida){
-//     var OK = re.exec(emailvalida.value);
-//     if (!OK){
-//         var alerta = document.getElementById("alerta2")
-//         alerta.style.display = 'block'
-//         alerta.innerHTML = 'Email invalido'
-//         setTimeout(function(){ alerta.style.display = 'none';}, 2000)
-//     }else{
-//         var alerta = document.getElementById("alerta")
-//         alerta.style.display = 'block'
-//         alerta.innerHTML = 'Email Valido'
-//         setTimeout(function(){ alerta.style.display = 'none';}, 1000)
-//     }
-//     }
+function testEmail(emailvalida){
+    var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    var OK = re.exec(emailvalida.value);
+    if (!OK){
+        var alerta = document.getElementById("alerta2")
+        alerta.style.display = 'block'
+        alerta.innerHTML = 'Email invalido'
+        setTimeout(function(){ alerta.style.display = 'none';}, 2000)
+    }else{
+        var alerta = document.getElementById("alerta")
+        alerta.style.display = 'block'
+        alerta.innerHTML = 'Email Valido'
+        setTimeout(function(){ alerta.style.display = 'none';}, 1000)
+    }
+}
+/* Mascara do telefone*/
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('phone').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
