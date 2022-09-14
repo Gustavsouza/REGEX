b7e42447beeb5c31efa93e7e730a8760ecba14f7
Gustavo De Souza <74631630+Gustavsouza@users.noreply.github.com>
2022-09-14 09:20:58 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 
 //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Regular_Expressions
-var re = /^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/;
+
 //   valida email aqui
 //   var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 function testInfo(phoneInput){
+var re = /^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/;
 var OK = re.exec(phoneInput.value);
 if (!OK){
     // window.alert(RegExp.input + " Não é um número de telefone com código de área!");
@@ -19,3 +20,21 @@ if (!OK){
     setTimeout(function(){ alerta.style.display = 'none';}, 2000)
 }
 }
+
+// /*Valida email*/
+
+// var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+// function testInfo(emailvalida){
+//     var OK = re.exec(emailvalida.value);
+//     if (!OK){
+//         var alerta = document.getElementById("alerta2")
+//         alerta.style.display = 'block'
+//         alerta.innerHTML = 'Email invalido'
+//         setTimeout(function(){ alerta.style.display = 'none';}, 2000)
+//     }else{
+//         var alerta = document.getElementById("alerta")
+//         alerta.style.display = 'block'
+//         alerta.innerHTML = 'Email Valido'
+//         setTimeout(function(){ alerta.style.display = 'none';}, 1000)
+//     }
+//     }
